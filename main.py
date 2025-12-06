51942ce37b5d6347c43c16d400fa45ca59e02ee3
Daniel <daniel.gallo12@gmail.com>
2025-12-05 16:41:27 -0800

    Renamed flask app file. Add python file of best model

--- main.py
+++ main.py
@@ -1,26 +1,45 @@
-from flask import Flask, render_template, request
-import pickle
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
 
-app = Flask(__name__)
-linreg = pickle.load(open('model.pkl','rb'))
+warnings.filterwarnings('ignore')
 
-@app.route('/')
-def home():
-    return render_template("home.html")
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import root_mean_squared_error
+import pickle
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    input = [float(x) for x in request.form.values()]
+"""
+This file is for creating the random forest model. 
+Testing which model and which split size was determined
+in the regression.ipynb notebook. 
+"""
 
-    input_array = np.array(input).reshape(1, -1)
+df = pd.read_csv('./data/cleaned_universities.csv')
 
-    output = linreg.predict(input_array)[0]
+df['school_type'] = df['school_type'].astype("category")
+# converts the school_type column to a cateogry dtype
 
-    return render_template('home.html', prediction_test=output)
-# def main():
-#     print("Hello from higher-education-consulting-analytics!")
+df['school_type_codes'] = df['school_type'].cat.codes
+# makes numerical codes for each school type
+# 0 = private
+# 1 = public
 
+X = df.drop(columns=['school_type', 'Graduation rate'])
+# independant features
+y = df['Graduation rate']
+# dependant feature
 
-if __name__ == "__main__":
-    app.run(debug=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+# split the data into testing and training datasets. 
+
+rf = RandomForestRegressor()
+rf.fit(X_train, y_train)
+
+# with open('model.pkl', 'wb') as file:
+#     pickle.dump(rf, file)
+
+# makes the pickle file for the model to load into. 
+# this is commented out so it doesn't get replaced every time
